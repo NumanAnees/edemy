cc7e6db5eafb7518ad8a0eea2cc6d3c6e7fd123e
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-10-03 03:00:02 +0500

    Database connected

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const { register } = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get("/register", (req, res) => {
-  res.send("hello register");
-});
+router.get("/register", register);
 
 module.exports = router;
--- Server/server.js
+++ Server/server.js
@@ -2,11 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const fs = require("fs");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 //create express app
 const app = express();
 
+//Db
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("***DB connected***");
+  })
+  .catch((err) => {
+    console.log("DB connection failed due to : ", err);
+  });
 //apply middlewares
 app.use(cors());
 app.use(express.json());
