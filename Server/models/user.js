3f63ebedec7b3afb004ad0ea5de2cbae54742f91
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-10-03 03:32:31 +0500

    user schema created

---
+++ Server/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 20,
+    },
+    picture: {
+      type: String,
+      default: "./avatar.png",
+    },
+    role: {
+      type: [String],
+      default: ["Subscriber"],
+      enum: ["Subscriber,Instructor,Admin"],
+    },
+    stripe_account_id: "",
+    stripe_seller: {},
+    stripeSession: {},
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
