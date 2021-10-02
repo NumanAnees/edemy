0b114e3ae58c390ba3300e4530a6e2fc2475e9eb
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-10-02 23:18:14 +0500

    register route

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,25 @@
-console.log("es");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const fs = require("fs");
+require("dotenv").config();
+
+//create express app
+const app = express();
+
+//apply middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//route
+fs.readdirSync("./routes").map((r) =>
+  app.use("/api", require(`./routes/${r}`))
+);
+//port
+const port = process.env.port || 8000;
+
+//server listening
+app.listen(port, () => {
+  console.log(`server is running at port" ${port}`);
+});
